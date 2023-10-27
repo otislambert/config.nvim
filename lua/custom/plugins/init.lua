d5da09e429d93fb9810d72fbc3b74c4d3a71899f
Otis Lambert <otislambert@gmail.com>
2023-10-27 13:33:06 -0700

    installed rust-tools

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,23 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	"simrat39/rust-tools.nvim",
+	dependencies = {
+		"neovim/nvim-lspconfig",
+		"nvim-lua/plenary.nvim",
+		"mfussenegger/nvim-dap",
+	},
+	config = function(_, _)
+		local rt = require("rust-tools")
+
+		rt.setup({
+			server = {
+				on_attach = function(_, bufnr)
+					vim.keymap.set("n", "<leader>t", rt.hover_actions.hover_actions, { buffer = bufnr })
+					vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+				end,
+			},
+		})
+	end,
+}
